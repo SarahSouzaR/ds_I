d4470ece3cce8858461ff75a3292bac8a1661959
SarahSouzaR <sarinha_desouza@outlook.com>
2018-05-13 19:24:04 -0300

    Funcionando

--- Calculadora/Calculadora_2/Calculadora_2/Form1.cs
+++ Calculadora/Calculadora_2/Calculadora_2/Form1.cs
@@ -23,14 +23,13 @@ namespace Calculadora_2
         {
             //Cria um provedor de Código C#
             CSharpCodeProvider mCodeProvider = new CSharpCodeProvider();
-            // Cria os parmaetros para a compilação origem
+            // Cria os parâmetros para a compilação origem
             CompilerParameters cp = new CompilerParameters();
             cp.GenerateExecutable = false;    //Não precisa criar um arquivo EXE
             cp.GenerateInMemory = true;       //Cria um na memória
             cp.OutputAssembly = "TempModule"; // Isso não é necessário, no entanto, se usado ​​repetidamente, faz com que o CLR não precisa carregar uma novo assembly cada vez que a função é executada.
             // A string abaixo é basicamente a casca do programa C #, que não faz nada, mas contém um método Avaliar() para nossos propósitos. 
-            //Atenção: Isso deixa a aplicação app aberto a ataques de injeção,
-            //Estou fazendo apenas para demonstração .
+       
             string TempModuleSource = "namespace ns{" +
                                       "using System;" +
                                       "class class1{" +

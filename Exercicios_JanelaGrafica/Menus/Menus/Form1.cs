f6b63fd0ceda04b8c9b38074c8ceeb7adc200dbe
SarahSouzaR <sarinha_desouza@outlook.com>
2018-05-29 20:26:50 -0300

    Testando menus com C#

---
+++ Exercicios_JanelaGrafica/Menus/Menus/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void teste1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTeste1 tst1 = new frmTeste1();
+            tst1.MdiParent = this; //define a instância do formulário criado (que é "filho" do form principal)
+            tst1.Show(); //exibe o formulário Teste1
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            lblData.Text = "Data: " + DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = "Hora: " + DateTime.Now.ToString("HH:mm:ss");
+        }
+
+       
+    }
+}
